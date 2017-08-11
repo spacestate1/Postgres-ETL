c9f4052fa2a019619fdc2defd8ae284fbe20fd0c
chiffre1 <cmcrann2@yahoo.com>
2017-08-11 09:59:27 -0700

    initial commit

---
+++ text_merge/merge_ps.sh
@@ -0,0 +1,43 @@
+ #!/bin/bash
+
+ directory2=$PWD/*
+ DATE2=`date +%Y-%m-%d:%H`
+ if [ ! -d "$PWD/extracted_av" ]; then
+ mkdir $PWD/extracted_av
+ fi
+
+ #///////////////// POLLSTORE COMBINER /////////////////
+
+ for F3 in $directory2
+    do
+    F3a=$(echo "$F3" | awk '{print tolower($0)}')
+    if [[ $F3a == *"11-"* ]]; then
+    if grep -Fxq "$F3a" import_record.txt
+        then
+        echo " File ${F3a} was already imported"
+	rm -rf ${F3a}
+	continue
+        fi
+    
+    new_file="pollstore_${DATE2}.csv"
+    echo $F3a >> import_record.txt
+    cat $F3a | sed '/agentIndex/d' | sed '/^\s*$/d' >> $new_file
+    echo "//// $new_file copied to $PWD/extracted_av ////"
+    rm -rf $F3a
+       fi
+    
+    done
+
+
+for F4 in $directory2
+    do
+    F4a=$(echo "$F4" | awk '{print tolower($0)}')
+    if [[ $F4a == *"pollstore"* ]]; then
+    new_file2="pollstore2_${DATE2}.csv"
+    echo $F4a >> import_record.txt
+    cat $F4a | sed '1s/^/agentIndex,pollIndex,time,value,alert,compression\n/' >> $new_file2
+    echo "//// $new_file2 copied to $PWD/extracted_av ////"
+    rm -rf $F4a
+    fi
+
+    done
